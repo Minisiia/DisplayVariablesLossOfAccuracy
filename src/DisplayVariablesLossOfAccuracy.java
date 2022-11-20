d5ad81d662a5162a7d64646bf2f286c3abff88a8
Minisiia <ar_tik@yahoo.com>
2022-11-20 14:13:46 +0200

    Java Start. 2. Machine mathematics. Data types

---
+++ src/DisplayVariablesLossOfAccuracy.java
@@ -0,0 +1,8 @@
+public class DisplayVariablesLossOfAccuracy {
+    public static void main(String[] args) {
+        double pi = 3.141592653d;
+        String e = "2.7182818284590452";        // використовуємо тип даних String, щоб не втратити останню цифру
+        System.out.println(pi);
+        System.out.println(e);
+    }
+}
